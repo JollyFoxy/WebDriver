815e5743b92d7a29e42a2befa97c61d3972f2895
pavel <pavel.mizirev@gmail.com>
2023-01-16 09:52:03 +0300

    Update 1

--- src/main/java/Steps/Xx.java
+++ src/main/java/Steps/Xx.java
@@ -1,2 +1,19 @@
-package Steps;public class Xx {
+package Steps;
+
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Xx {
+
+    @BeforeClass
+    static void setUp(){
+        System.setProperty("webdriver.chrome.driver",ConfProperties.getProperties("chrome driver"));
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperties("loginpage"));
+    }
 }
